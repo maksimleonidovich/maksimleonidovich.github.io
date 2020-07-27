1c8345c86e14f0ced81584b9de65b154aa11c71d
maksimleonidovich <68861866+maksimleonidovich@users.noreply.github.com>
2020-07-27 18:49:27 +0300

    Galam2

---
+++ JQuery.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    /* Smooth scroll */
+	$("[data-scroll]").on("click", function (event) {
+		event.preventDefault();
+
+		var $this = $(this),
+			blockId = $this.data('scroll'),
+			blockOffset = $(blockId).offset().top;
+
+
+		$("html, body").animate({
+			scrollTop: blockOffset
+		}, 1000);
+	});
+
+});
